fc19158f30cb23ede981bc2b56c60317ade81652
Lindsay Ward <lindsay.ward@jcu.edu.au>
2025-09-25 19:05:17 +1000

    Add online seminar 2 code

---
+++ online/seminar02.py
@@ -0,0 +1,15 @@
+"""
+<priming read - get some input>
+while <input is bad>
+    display error message
+    <same as the priming read again - get some input>
+do next thing now that you know the input is valid
+"""
+from random import randint
+
+low = int(input("Low: "))
+high = int(input("High: "))
+while low >= high:
+    print(f"Invalid. High number must be > {low}")
+    high = int(input("High: "))
+print(randint(low, high))
