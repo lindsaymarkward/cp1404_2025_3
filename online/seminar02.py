118803051b8fac14e43d25c44ffbc7a34f31e4df
Lindsay Ward <lindsay.ward@jcu.edu.au>
2025-09-25 19:08:23 +1000

    Refactor online seminar 2 code to use main function

--- online/seminar02.py
+++ online/seminar02.py
@@ -7,9 +7,14 @@ do next thing now that you know the input is valid
 """
 from random import randint
 
-low = int(input("Low: "))
-high = int(input("High: "))
-while low >= high:
-    print(f"Invalid. High number must be > {low}")
+
+def main():
+    low = int(input("Low: "))
     high = int(input("High: "))
-print(randint(low, high))
+    while low >= high:
+        print(f"Invalid. High number must be > {low}")
+        high = int(input("High: "))
+    print(randint(low, high))
+
+
+main()
