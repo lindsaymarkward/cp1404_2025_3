66967fac9b337c796e32c20d9a5cc49f5cfe5c3b
Lindsay Ward <lindsay.ward@jcu.edu.au>
2025-10-14 10:16:49 +1000

    Complete dictionary conversion functions

--- seminar05.py
+++ seminar05.py
@@ -1,1 +1,20 @@
 """Seminar 5 - Dictionaries"""
+
+
+def main():
+    print(convert_to_dictionary(["a", "bbb", "longer string", ""]))
+    print(convert_to_dictionary2(["a", "bbb", "longer string", ""]))
+
+
+def convert_to_dictionary2(strings):
+    return {string: len(string) for string in strings}
+
+
+def convert_to_dictionary(strings):
+    string_to_length = {}
+    for string in strings:
+        string_to_length[string] = len(string)
+    return string_to_length
+
+
+main()
